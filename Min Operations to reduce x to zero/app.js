175d7b1d6f82ca33ee401fad569183a5a44d8bda
Mustafa Hasan <mustafa2k8972@gmail.com>
2023-09-23 18:59:24 +0530

    Added README.md

---
+++ Min Operations to reduce x to zero/app.js
@@ -0,0 +1,10 @@
+/**
+ * Find the minimum number of operations to make the sum of a subarray of nums equal to x.
+ *
+ * @param {number[]} nums - The array of numbers.
+ * @param {number} x - The target sum.
+ * @return {number} The minimum number of operations.
+ */
+var minOperations = function (nums, x) {
+  s;
+};
---
+++ README.md
@@ -0,0 +1,1 @@
+## This repository contains all the problems solved by me from LeetCode
