6d521ccc72a75cd4306d6494b987a97cf8296b39
Mustafa Hasan <mustafa2k8972@gmail.com>
2023-09-23 23:21:02 +0530

    Problem Solved

--- Min Operations to reduce x to zero/app.js
+++ Min Operations to reduce x to zero/app.js
@@ -5,4 +5,29 @@
  * @param {number} x - The target sum.
  * @return {number} The minimum number of operations.
  */
-var minOperations = function (nums, x) {};
+var minOperations = function (nums, x) {
+  const total = nums.reduce((acc, curr) => acc + curr, 0);
+  const target = total - x;
+
+  let left = 0;
+  let right = 0;
+  let currSum = 0;
+  let minOps = Infinity;
+  for (right; right < nums.length; right++) {
+    currSum += nums[right];
+
+    while (currSum > target && left <= right) {
+      currSum = currSum - nums[left];
+      left++;
+    }
+
+    if (currSum === target) {
+      minOps = Math.min(minOps, nums.length - (right - left + 1));
+    }
+  }
+  if (minOps === Infinity) {
+    return -1;
+  } else {
+    return minOps;
+  }
+};
