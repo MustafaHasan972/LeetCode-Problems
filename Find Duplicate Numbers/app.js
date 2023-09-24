5ce5e52b0b6808fb1c935a406b7be5ee0b40e564
Mustafa Hasan <mustafa2k8972@gmail.com>
2023-09-24 10:54:24 +0530

    Problem Solved

--- Find Duplicate Numbers/app.js
+++ Find Duplicate Numbers/app.js
@@ -1,1 +1,22 @@
-var findDuplicate = function (nums) {};
+/**
+ * Finds and returns the first duplicate number in the given array.
+ *
+ * @param {array} nums - An array of numbers to search for duplicates.
+ * @return {number|undefined} The first duplicate number found, or undefined if no duplicates are found.
+ */
+function findDuplicate(nums) {
+  // Sort the array in ascending order
+  const sorted = nums.sort((a, b) => a - b);
+
+  // Iterate through the sorted array
+  for (let i = 0; i < sorted.length; i++) {
+    // Check if the current number is equal to the next number
+    if (sorted[i] === sorted[i + 1]) {
+      // If a duplicate is found, return the duplicate number
+      return sorted[i];
+    }
+  }
+
+  // If no duplicate is found, return undefined
+  return undefined;
+}
