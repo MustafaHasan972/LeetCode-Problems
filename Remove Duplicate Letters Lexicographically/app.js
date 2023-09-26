5629bdf1ae07d6477f3ae7e8c0be5cca1f86b65c
Mustafa Hasan <mustafa2k8972@gmail.com>
2023-09-26 14:25:44 +0530

    Added verbose comments

--- Remove Duplicate Letters Lexicographically/app.js
+++ Remove Duplicate Letters Lexicographically/app.js
@@ -1,20 +1,32 @@
 /**
- * @param {string} s
- * @return {string}
+ * Remove duplicate letters from a string while maintaining the lexicographical order.
+ *
+ * @param {string} s - The input string.
+ * @return {string} - The string with duplicate letters removed.
  */
-var removeDuplicateLetters = function (s) {
+const removeDuplicateLetters = function (s) {
+  // Create a stack to store the result.
   let stack = [];
+
+  // Create a set to keep track of seen characters.
   let seen = new Set();
+
+  // Create an object to store the last occurrence of each character.
   let lastOccurrence = {};
 
+  // Iterate through the input string to find the last occurrence of each character.
   for (let i = 0; i < s.length; i++) {
     lastOccurrence[s[i]] = i;
   }
 
+  // Iterate through the input string again.
   for (let i = 0; i < s.length; i++) {
     const char = s[i];
 
+    // If the character has not been seen before, process it.
     if (!seen.has(char)) {
+      // Remove any characters from the stack that are greater than the current character
+      // and occur later in the string.
       while (
         stack.length > 0 &&
         char < stack[stack.length - 1] &&
@@ -22,10 +34,14 @@ var removeDuplicateLetters = function (s) {
       ) {
         seen.delete(stack.pop());
       }
+
+      // Add the current character to the stack and mark it as seen.
       seen.add(char);
       stack.push(char);
     }
   }
+
+  // Return the result by joining the characters in the stack.
   return stack.join("");
 };
 
