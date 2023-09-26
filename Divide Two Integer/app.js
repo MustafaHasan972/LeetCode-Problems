9790d258a9e640eb57b68c22f6379d00c8705310
Mustafa Hasan <mustafa2k8972@gmail.com>
2023-09-26 21:11:56 +0530

    Modified verbose comments

--- Divide Two Integer/app.js
+++ Divide Two Integer/app.js
@@ -1,32 +1,38 @@
-/**
- * @param {number} dividend
- * @param {number} divisor
- * @return {number}
- */
+// Refactored code to improve readability and maintainability
 
-var divide = function (dividend, divisor) {
-  let intMax = Math.pow(2, 31) - 1;
-  let intMin = -Math.pow(2, 31);
-  let quotient = 0;
+const divide = function (A, B) {
+  // Handle edge case where dividend is the minimum value and divisor is -1
+  if (A === -2147483648 && B === -1) return 2147483647;
 
-  if (divisor === 0 || dividend === 0) return 0;
+  // Initialize variables
+  let ans = 0;
+  let sign = 1;
 
-  if (dividend === intMin && divisor === -1) return intMax;
-
-  const isNegative =
-    (dividend < 0 && divisor > 0) || (dividend > 0 && divisor < 0);
-
-  let positiveDividend = Math.abs(dividend);
-  let positiveDivisor = Math.abs(divisor);
-
-  while (positiveDividend >= positiveDivisor) {
-    positiveDividend -= positiveDivisor;
-    quotient++;
+  // Check if dividend and divisor are negative
+  if (A < 0) {
+    A = -A;
+    sign = -sign;
+  }
+  if (B < 0) {
+    B = -B;
+    sign = -sign;
   }
 
-  if (isNegative) quotient -= quotient;
+  // Handle special cases where dividend is equal to divisor
+  if (A === B) return sign;
 
-  return quotient;
+  // Perform division using bit manipulation
+  for (let i = 0, val = B; A >= B; i = 0, val = B) {
+    // Find the largest multiple of divisor that is less than or equal to dividend using left shift
+    while (val > 0 && val <= A) {
+      val = B << ++i;
+    }
+
+    // Subtract the multiple from dividend and update quotient
+    A -= B << (i - 1);
+    ans += 1 << (i - 1);
+  }
+
+  // Apply sign to the quotient
+  return sign < 0 ? -ans : ans;
 };
-
-console.log(divide(7, -3));
