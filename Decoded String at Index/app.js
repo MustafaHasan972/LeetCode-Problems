b2f9e10b60b8e0343193e39a1418895bdb7ba701
Mustafa Hasan <mustafa2k8972@gmail.com>
2023-09-27 17:58:40 +0530

    Added verbose comments

--- Decoded String at Index/app.js
+++ Decoded String at Index/app.js
@@ -6,29 +6,34 @@
  * @return {string} The decoded character at index `k`.
  */
 let decodeAtIndex = function (s, k) {
-  let length = 0;
-  let i = 0;
+  let length = 0; // Initialize the length of the decoded string
+  let i = 0; // Initialize the index of the current character
 
+  // Calculate the length of the decoded string until it reaches the desired index
   while (length < k) {
     if (!isNaN(s[i])) {
-      length *= Number(s[i]);
+      // Check if the current character is a number
+      length *= Number(s[i]); // Multiply the length by the number
     } else {
-      length++;
+      length++; // Increment the length by 1
     }
-    i++;
+    i++; // Move to the next character
   }
 
+  // Iterate backwards from the current index to find the decoded character at index `k`
   for (let j = i - 1; j >= 0; j--) {
     if (!isNaN(s[j])) {
-      length /= Number(s[j]);
-      k %= length;
+      // Check if the current character is a number
+      length /= Number(s[j]); // Divide the length by the number
+      k %= length; // Update the index by taking the modulo of k and the length
     } else {
       if (k === 0 || k === length) {
-        return s[j];
+        // Check if the index is either 0 or equal to the length
+        return s[j]; // Return the decoded character
       }
-      length--;
+      length--; // Decrement the length by 1
     }
   }
 
-  return "";
+  return ""; // Return an empty string if the decoded character is not found
 };
