f351952b2937e5d0e2289a3744137fe857f73255
Mustafa Hasan <mustafa2k8972@gmail.com>
2023-10-01 10:34:52 +0530

    Problem Sovled

--- Reverse Words in a String III/app.js
+++ Reverse Words in a String III/app.js
@@ -2,4 +2,18 @@
  * @param {string} s
  * @return {string}
  */
-var reverseWords = function (s) {};
+var reverseWords = function (s) {
+  const arr = s.split(" ");
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = arr[i].split("").reverse().join("");
+  }
+  return arr.join(" ");
+
+  // Another Method to solve this problem
+  //   return s
+  //     .split(" ")
+  //     .map((word) => word.split("").reverse().join(""))
+  //     .join(" ");
+};
+
+console.log(reverseWords("Let's take LeetCode contest"));
