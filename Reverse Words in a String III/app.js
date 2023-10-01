4decc1c3e54c7113f465b35a4afde4a3cda00048
Mustafa Hasan <mustafa2k8972@gmail.com>
2023-10-01 10:40:25 +0530

    Added verbose comments

--- Reverse Words in a String III/app.js
+++ Reverse Words in a String III/app.js
@@ -1,19 +1,21 @@
 /**
- * @param {string} s
- * @return {string}
+ * Reverses the order of characters in each word of a given string.
+ *
+ * @param {string} s - The input string.
+ * @return {string} - The string with reversed words.
  */
-var reverseWords = function (s) {
+function reverseWords(s) {
+  // Split the input string into an array of words
   const arr = s.split(" ");
+
+  // Iterate through each word in the array
   for (let i = 0; i < arr.length; i++) {
+    // Split the word into an array of characters, reverse the order, and join them back into a string
     arr[i] = arr[i].split("").reverse().join("");
   }
-  return arr.join(" ");
 
-  // Another Method to solve this problem
-  //   return s
-  //     .split(" ")
-  //     .map((word) => word.split("").reverse().join(""))
-  //     .join(" ");
-};
+  // Join the array of reversed words back into a string, separated by spaces
+  return arr.join(" ");
+}
 
 console.log(reverseWords("Let's take LeetCode contest"));
