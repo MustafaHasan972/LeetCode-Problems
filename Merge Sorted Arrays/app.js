29760832609364e7e7c0f956e51cf3e28fe0e8a2
Mustafa Hasan <mustafa2k8972@gmail.com>
2023-10-01 23:46:24 +0530

    Problem solved

--- Merge Sorted Arrays/app.js
+++ Merge Sorted Arrays/app.js
@@ -5,4 +5,11 @@
  * @param {number} n
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
-const merge = function (nums1, m, nums2, n) {};
+const merge = function (nums1, m, nums2, n) {
+  for (let i = m; i < m + n; i++) {
+    nums1[i] = nums2[i - m];
+  }
+  return nums1.sort((a, b) => a - b);
+};
+
+console.log(merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3));
