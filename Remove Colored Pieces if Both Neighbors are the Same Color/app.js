63d46c5d30a51467f7be6cf746077599769c993e
Mustafa Hasan <mustafa2k8972@gmail.com>
2023-10-02 12:55:53 +0530

    Problem Solved

--- Remove Colored Pieces if Both Neighbors are the Same Color/app.js
+++ Remove Colored Pieces if Both Neighbors are the Same Color/app.js
@@ -2,4 +2,27 @@
  * @param {string} colors
  * @return {boolean}
  */
-var winnerOfGame = function (colors) {};
+const winnerOfGame = (colors) => {
+  let countA = 0;
+  let countB = 0;
+
+  for (let i = 0; i < colors.length; i++) {
+    let x = colors[i];
+    let count = 0;
+
+    while (i < colors.length && colors[i] === x) {
+      i++;
+      count++;
+    }
+
+    if (x === "A") {
+      countA += Math.max(count - 2, 0);
+    } else if (x === "B") {
+      countB += Math.max(count - 2, 0);
+    }
+  }
+
+  return countA > countB;
+};
+
+console.log(winnerOfGame("AAABABB")); // This will now return true
