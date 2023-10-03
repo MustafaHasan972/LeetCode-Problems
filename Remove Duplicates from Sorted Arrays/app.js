b515a48957c9148d2926aee4c27ba264c6d2e169
Mustafa Hasan <mustafa2k8972@gmail.com>
2023-10-03 10:54:52 +0530

    Problem Solved

--- Remove Duplicates from Sorted Arrays/app.js
+++ Remove Duplicates from Sorted Arrays/app.js
@@ -2,6 +2,15 @@
  * @param {number[]} nums
  * @return {number}
  */
-var removeDuplicates = function (nums) {};
+var removeDuplicates = function (nums) {
+  let i = 0;
+  for (let j = 1; j < nums.length; j++) {
+    if (nums[i] !== nums[j]) {
+      i++;
+      nums[i] = nums[j];
+    }
+  }
+  return i + 1;
+};
 
 console.log(removeDuplicates([1, 1, 2]));
