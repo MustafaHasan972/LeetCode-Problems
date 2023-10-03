08264016a5bd27d4d2f1b44695e265d45a3fed4e
Mustafa Hasan <mustafa2k8972@gmail.com>
2023-10-03 10:40:58 +0530

    Problem solved

--- Number of Good Pairs/app.js
+++ Number of Good Pairs/app.js
@@ -2,4 +2,16 @@
  * @param {number[]} nums
  * @return {number}
  */
-var numIdenticalPairs = function (nums) {};
+var numIdenticalPairs = (nums) => {
+  let count = 0;
+  for (let i = 0; i < nums.length - 1; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] === nums[j]) {
+        count++;
+      }
+    }
+  }
+  return count;
+};
+
+console.log(numIdenticalPairs([1, 2, 3, 1, 1, 3]));
