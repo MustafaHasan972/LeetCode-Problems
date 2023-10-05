ae37c8b1127eeedcdb0313dd93be1ab458434408
Mustafa Hasan <mustafa2k8972@gmail.com>
2023-10-05 23:26:02 +0530

    Added verbose comments

--- Majority ELements II/app.js
+++ Majority ELements II/app.js
@@ -1,13 +1,15 @@
 /**
- * @param {number[]} nums
- * @return {number[]}
+ * @param {number[]} nums - An array of integers.
+ * @return {number[]} - Returns an array containing the majority elements (elements that appear more than ⌊n / 3⌋ times).
  */
 var majorityElement = function (nums) {
+  // Initialize variables for two potential candidates and their respective counts.
   let candidate1 = null;
   let candidate2 = null;
   let count1 = 0;
   let count2 = 0;
 
+  // First pass through the array to identify potential candidates.
   for (let num of nums) {
     if (candidate1 === num) {
       count1 += 1;
@@ -20,13 +22,17 @@ var majorityElement = function (nums) {
       candidate2 = num;
       count2 = 1;
     } else {
+      // Decrement counts if the current number doesn't match any candidate.
       count1 -= 1;
       count2 -= 1;
     }
   }
 
+  // Reset counts for both candidates.
   count1 = 0;
   count2 = 0;
+
+  // Second pass through the array to count the occurrences of the potential candidates.
   for (let num of nums) {
     if (candidate1 === num) {
       count1 += 1;
@@ -35,14 +41,19 @@ var majorityElement = function (nums) {
     }
   }
 
+  // Initialize an array to store the majority elements.
   let result = [];
+
+  // Check if the candidates appear more than ⌊n / 3⌋ times and add them to the result array if so.
   if (count1 > nums.length / 3) {
     result.push(candidate1);
-  } else if (count2 > nums.length / 3) {
+  }
+  if (count2 > nums.length / 3) {
     result.push(candidate2);
   }
 
   return result;
 };
 
+// Test the function with an example array and print the result.
 console.log(majorityElement([3, 2, 3]));
