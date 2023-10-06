8e5a1cb39805d2770b0f3c3305a84d2d6310fde7
Mustafa Hasan <mustafa2k8972@gmail.com>
2023-10-06 12:35:04 +0530

    Problem Solved

--- Rotate Array/app.js
+++ Rotate Array/app.js
@@ -3,4 +3,22 @@
  * @param {number} k
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-var rotate = function (nums, k) {};
+var rotate = function (nums, k) {
+  k %= nums.length;
+
+  const reverse = (i, j) => {
+    while (i < j) {
+      let temp = nums[i];
+      nums[i] = nums[j];
+      nums[j] = temp;
+      i++;
+      j--;
+    }
+  };
+
+  reverse(0, nums.length - 1);
+  reverse(0, k - 1);
+  reverse(k, nums.length - 1);
+};
+
+console.log(rotate([1, 2, 3, 4, 5, 6, 7], 3));
