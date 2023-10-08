85aaed5a28ea2d4f977f372c26dfd70e06365102
Mustafa Hasan <mustafa2k8972@gmail.com>
2023-10-08 21:10:41 +0530

    Initialized files

---
+++ Max dot product of Two Subsequence/app.js
@@ -0,0 +1,8 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var maxDotProduct = function (nums1, nums2) {};
+
+console.log(maxDotProduct([1, 2, -2, 5], [3, 0, -6]));
