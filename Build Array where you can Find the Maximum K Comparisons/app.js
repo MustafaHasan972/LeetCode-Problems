100483b73a7a96254d379d93536b430243938ee8
Mustafa Hasan <mustafa2k8972@gmail.com>
2023-10-08 21:03:52 +0530

    Problem Solved

--- Build Array where you can Find the Maximum K Comparisons/app.js
+++ Build Array where you can Find the Maximum K Comparisons/app.js
@@ -1,9 +1,34 @@
-/**
- * @param {number} n
- * @param {number} m
- * @param {number} k
- * @return {number}
- */
-var numOfArrays = function (n, m, k) {};
+const numOfArrays = function (n, m, k) {
+  const mod = 1e9 + 7;
 
-console.log(numOfArrays(2, 3, 1));
+  let dp = Array.from({ length: m + 1 }, () => Array(k + 1).fill(0));
+  let prefix = Array.from({ length: m + 1 }, () => Array(k + 1).fill(0));
+  let prevDp = Array.from({ length: m + 1 }, () => Array(k + 1).fill(0));
+  let prevPrefix = Array.from({ length: m + 1 }, () => Array(k + 1).fill(0));
+
+  for (let j = 1; j <= m; j++) {
+    prevDp[j][1] = 1;
+    prevPrefix[j][1] = j;
+  }
+
+  for (let _ = 2; _ <= n; _++) {
+    for (let maxNum = 1; maxNum <= m; maxNum++) {
+      for (let cost = 1; cost <= k; cost++) {
+        dp[maxNum][cost] = (maxNum * prevDp[maxNum][cost]) % mod;
+
+        if (maxNum > 1 && cost > 1) {
+          dp[maxNum][cost] =
+            (dp[maxNum][cost] + prevPrefix[maxNum - 1][cost - 1]) % mod;
+        }
+
+        prefix[maxNum][cost] =
+          (prefix[maxNum - 1][cost] + dp[maxNum][cost]) % mod;
+      }
+    }
+
+    prevDp = dp;
+    prevPrefix = prefix;
+  }
+
+  return prefix[m][k];
+};
