11ee3da42021250c80dff7f5d1a1c844e8ce3c50
Mustafa Hasan <mustafa2k8972@gmail.com>
2023-10-09 17:37:26 +0530

    Problem Solved

--- Best Time to Buy and Sell Stocks II/app.js
+++ Best Time to Buy and Sell Stocks II/app.js
@@ -1,12 +1,13 @@
 function maxProfit(prices) {
-  //   let profit = 0;
-  //   let i = 0;
-  //   for (let i = 1; i <= prices.length - 1; i++) {
-  //     if (prices[i] > prices[i - 1]) {
-  //       profit += prices[i] - prices[i - 1];
-  //     }
-  //   }
-  //   return profit;
+  let profit = 0;
+  let i = 0;
+
+  for (let i = 1; i <= prices.length - 1; i++) {
+    if (prices[i] > prices[i - 1]) {
+      profit += prices[i] - prices[i - 1];
+    }
+  }
+  return profit;
 }
 
 console.log(maxProfit([7, 1, 5, 3, 6, 4]));
