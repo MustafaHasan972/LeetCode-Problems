4ea36b1ba2fdcd93a3b92038235856cd5e2dcddf
Mustafa Hasan <mustafa2k8972@gmail.com>
2023-10-09 17:37:00 +0530

    Initialized fiels

---
+++ Best Time to Buy and Sell Stocks II/app.js
@@ -0,0 +1,12 @@
+function maxProfit(prices) {
+  //   let profit = 0;
+  //   let i = 0;
+  //   for (let i = 1; i <= prices.length - 1; i++) {
+  //     if (prices[i] > prices[i - 1]) {
+  //       profit += prices[i] - prices[i - 1];
+  //     }
+  //   }
+  //   return profit;
+}
+
+console.log(maxProfit([7, 1, 5, 3, 6, 4]));
