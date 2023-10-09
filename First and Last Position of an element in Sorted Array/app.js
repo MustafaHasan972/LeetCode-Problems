892b41b955e42cbea198b71d764dd7ef7fb53e46
Mustafa Hasan <mustafa2k8972@gmail.com>
2023-10-09 07:56:32 +0530

    Problem solved

--- First and Last Position of an element in Sorted Array/app.js
+++ First and Last Position of an element in Sorted Array/app.js
@@ -3,6 +3,43 @@
  * @param {number} target
  * @return {number[]}
  */
-var searchRange = function (nums, target) {};
+var searchRange = function (nums, target) {
+  let first = -1;
+  let last = -1;
 
-consnole.log(searchRange([5, 7, 7, 8, 8, 10], 8));
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+
+    if (nums[mid] === target) {
+      first = mid;
+      right = mid - 1;
+    } else if (nums[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  left = 0;
+  right = nums.length - 1;
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+
+    if (nums[mid] === target) {
+      last = mid;
+      left = mid + 1;
+    } else if (nums[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  return [first, last];
+};
+
+console.log(searchRange([5, 7, 7, 8, 8, 10], 8));
