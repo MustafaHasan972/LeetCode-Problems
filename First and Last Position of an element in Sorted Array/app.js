3b4798b554cf9d8b42095a22688c94da3089063a
Mustafa Hasan <mustafa2k8972@gmail.com>
2023-10-09 07:34:44 +0530

    Initialized Files

---
+++ First and Last Position of an element in Sorted Array/app.js
@@ -0,0 +1,8 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var searchRange = function (nums, target) {};
+
+consnole.log(searchRange([5, 7, 7, 8, 8, 10], 8));
