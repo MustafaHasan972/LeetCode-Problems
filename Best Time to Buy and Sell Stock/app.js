a4adf1c6abbe27441e3819b747893a4850ef196f
Mustafa Hasan <mustafa2k8972@gmail.com>
2023-10-09 11:27:25 +0530

    Problem Solved

--- Best Time to Buy and Sell Stock/app.js
+++ Best Time to Buy and Sell Stock/app.js
@@ -4,6 +4,21 @@
  * @param {Array<number>} prices - An array of stock prices.
  * @return {number} The maximum profit possible.
  */
-const maxProfit = function (prices) {};
+const maxProfit = function (prices) {
+  let minPrice = Infinity;
+  let maxProfit = 0;
+
+  for (let i = 0; i < prices.length; i++) {
+    if (prices[i] < minPrice) {
+      minPrice = prices[i];
+    } else {
+      let potentialProfit = prices[i] - minPrice;
+      if (potentialProfit > maxProfit) {
+        maxProfit = potentialProfit;
+      }
+    }
+  }
+  return maxProfit;
+};
 
 console.log(maxProfit([7, 1, 5, 3, 6, 4]));
