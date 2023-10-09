1a2dcc96d71816f849ff9369bdcbeeb159e96cf2
Mustafa Hasan <mustafa2k8972@gmail.com>
2023-10-09 11:28:12 +0530

    Added verbose comments

--- Best Time to Buy and Sell Stock/app.js
+++ Best Time to Buy and Sell Stock/app.js
@@ -1,24 +1,30 @@
 /**
- * Calculates the maximum profit that can be obtained by buying and selling stocks.
- *
- * @param {Array<number>} prices - An array of stock prices.
- * @return {number} The maximum profit possible.
+ * @param {number[]} prices - An array of stock prices.
+ * @return {number} - Returns the maximum profit that can be obtained.
  */
-const maxProfit = function (prices) {
-  let minPrice = Infinity;
-  let maxProfit = 0;
+var maxProfit = function (prices) {
+  // Initialize variables to keep track of the minimum price and maximum profit.
+  let minPrice = Infinity; // Set to positive infinity as an initial placeholder.
+  let maxProfit = 0; // Initialize the maximum profit to 0.
 
+  // Iterate through the array of stock prices.
   for (let i = 0; i < prices.length; i++) {
     if (prices[i] < minPrice) {
+      // If the current price is less than the minimum price seen so far,
+      // update the minimum price to the current price.
       minPrice = prices[i];
     } else {
+      // If the current price is greater than or equal to the minimum price,
+      // calculate the potential profit by selling at the current price.
       let potentialProfit = prices[i] - minPrice;
+
+      // Update the maximum profit if the potential profit is greater.
       if (potentialProfit > maxProfit) {
         maxProfit = potentialProfit;
       }
     }
   }
+
+  // The maximum profit is stored in the 'maxProfit' variable.
   return maxProfit;
 };
-
-console.log(maxProfit([7, 1, 5, 3, 6, 4]));
