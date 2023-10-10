ff582b6bba1b7f0d2ede61de3fb010d308febd90
Mustafa Hasan <mustafa2k8972@gmail.com>
2023-10-10 22:55:55 +0530

    Added verbose comments

--- Minimum Number of Operation to make Array Continous/app.js
+++ Minimum Number of Operation to make Array Continous/app.js
@@ -1,27 +1,32 @@
 /**
- * @param {number[]} nums
- * @return {number}
+ * @param {number[]} nums - An array of integers.
+ * @return {number} - Returns the minimum number of operations needed to make the elements unique.
  */
-var minOperations = function (nums) {
-  nums.sort((a, b) => a - b);
+function minOperations(nums) {
+  const n = nums.length; // Get the length of the input array 'nums'.
+  const uniqueSet = new Set(); // Create a Set to store unique elements.
+  let maxContinuous = 0; // Initialize a variable to track the maximum continuous subarray length.
 
-  const n = nums.length;
-  const maxPossibleLength = nums[n - 1] - nums[0] + 1;
-
-  let minOperations = n - maxPossibleLength;
-
-  for (let right = 1, left = 0; right < n; right++) {
-    const currentLength = right - left + 1;
-    const elementsToRemove = currentLength - 1;
-    let m = nums[right] - nums[left] + 1;
-
-    if (m > currentLength) {
-      minOperations = Math.min(minOperations, elementsToRemove);
-      left++;
-    }
+  // Add all elements of 'nums' to the Set 'uniqueSet' to remove duplicates.
+  for (let num of nums) {
+    uniqueSet.add(num);
   }
 
-  return minOperations;
-};
+  // Convert the Set 'uniqueSet' back to an array and sort it in ascending order.
+  const uniqueNums = Array.from(uniqueSet).sort((a, b) => a - b);
 
-console.log(minOperations([4, 2, 2, 2, 4, 4, 2, 2]));
+  // Use two pointers approach to find the maximum continuous subarray length.
+  for (let left = 0, right = 0; right < uniqueNums.length; right++) {
+    // While the difference between the elements at 'right' and 'left' is greater than 'n - 1',
+    // increment 'left' to reduce the subarray length.
+    while (uniqueNums[right] - uniqueNums[left] > n - 1) {
+      left++;
+    }
+    // Update 'maxContinuous' with the maximum subarray length.
+    maxContinuous = Math.max(maxContinuous, right - left + 1);
+  }
+
+  // The minimum number of operations is the difference between the total number of elements 'n'
+  // and the maximum continuous subarray length.
+  return n - maxContinuous;
+}
