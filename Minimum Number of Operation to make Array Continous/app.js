39a995fcb5fcbd9c8612c87c496f5488ffe4e809
Mustafa Hasan <mustafa2k8972@gmail.com>
2023-10-10 22:55:22 +0530

    Problem Solved

--- Minimum Number of Operation to make Array Continous/app.js
+++ Minimum Number of Operation to make Array Continous/app.js
@@ -2,4 +2,26 @@
  * @param {number[]} nums
  * @return {number}
  */
-var minOperations = function (nums) {};
+var minOperations = function (nums) {
+  nums.sort((a, b) => a - b);
+
+  const n = nums.length;
+  const maxPossibleLength = nums[n - 1] - nums[0] + 1;
+
+  let minOperations = n - maxPossibleLength;
+
+  for (let right = 1, left = 0; right < n; right++) {
+    const currentLength = right - left + 1;
+    const elementsToRemove = currentLength - 1;
+    let m = nums[right] - nums[left] + 1;
+
+    if (m > currentLength) {
+      minOperations = Math.min(minOperations, elementsToRemove);
+      left++;
+    }
+  }
+
+  return minOperations;
+};
+
+console.log(minOperations([4, 2, 2, 2, 4, 4, 2, 2]));
